a256f3add710f6e11abd3b3c0f6e6118e101ae0e
John Chiki <Tylervette2016@gmail.com>
2021-11-28 15:46:16 -0700

    Fixxed space

--- Quiz/server/index.js
+++ Quiz/server/index.js
@@ -19,3 +19,4 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message)); 
+    
