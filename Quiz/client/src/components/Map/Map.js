08836128ece4c9bba3c2bdf63768602a36dd2f08
jakeo <jake.oard@gmail.com>
2021-12-03 18:53:42 -0700

    got game workin'; added more pages games

--- Quiz/client/src/components/Map/Map.js
+++ Quiz/client/src/components/Map/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Counties from './Counties/counties.jsx';
+import FindCounties from './Counties/findCounties'
 import './App.css';
 
 class Map extends Component {
@@ -8,7 +9,7 @@ class Map extends Component {
         <header className="App-header">
           <React.Fragment>
             <div id="Map_Div">
-              <Counties/>
+              <FindCounties/>
             </div>
           </React.Fragment>
         </header>
